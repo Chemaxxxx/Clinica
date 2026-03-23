b2a09508d12066fc9f8b23574653d9278674b632
Mireiamoron <miremoroncid@gmail.com>
2026-03-23 18:13:20 +0100

    Add files via upload

---
+++ Propietario.java
@@ -0,0 +1,16 @@
+package Clinica;
+
+public class Propietario {
+
+	private String nombre;
+	private String telefono;
+	
+	public Propietario (String nombre, String telefono) {
+		this.nombre = nombre;
+		this.telefono = telefono;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+}
