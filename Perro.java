615715c1f63b10e2c5e5eefaaae2226e9b6e8bd6
mpedgar467 <mpedgar467@g.educaand.es>
2026-03-24 20:25:23 +0100

    Add files via upload

--- Perro.java
+++ Perro.java
@@ -2,9 +2,25 @@ package Clinica;
 
 public class Perro extends Domestico {
 
-	public Perro(String nombre, int edad, String raza, String chip) {
-		super(nombre, edad, raza, chip);
 	
+
+	
+
+	public Perro(String nombre, int edad, String raza, String chip, Sexo sexo, int peso, Propietario p) {
+		super(nombre, edad, raza, chip, sexo, peso, p);
+	
+	}
+
+	@Override
+	public void cortarUnas() {
+		
+		
+	}
+
+	@Override
+	public void contactaPropietario() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
