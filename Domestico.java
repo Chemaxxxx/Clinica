0d9f31f847e65e4080b1e4b04daf9d7fc178b300
mpedgar467 <mpedgar467@g.educaand.es>
2026-03-24 20:27:50 +0100

    Add files via upload

--- Domestico.java
+++ Domestico.java
@@ -1,17 +1,42 @@
 package Clinica;
 
-abstract class Domestico {
+abstract class Domestico extends Animales {
+
 	protected String nombre;
 	protected int edad;
 	protected String raza;
 	protected String chip;
 	
-	public Domestico(String nombre, int edad, String raza, String chip) {
-		this.nombre = nombre;
-		this.edad = edad;
-		this.raza = raza;
-		this.chip = chip;
+	public Domestico(String nombre, int edad, String raza, String chip,Sexo sexo, int peso, Propietario p) {
+		super(sexo, peso, p);
+	this.nombre=nombre;
+	this.edad=edad;
+	this.raza=raza;
+	this.chip=chip;
 	}
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+	
+
+	
+	public abstract void cortarUnas();
+	
 	
 	
 
