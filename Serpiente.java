fc04a873945cffc2d405be354aa388455ff5046f
Mireiamoron <miremoroncid@gmail.com>
2026-03-25 19:45:20 +0100

    Add files via upload

--- Serpiente.java
+++ Serpiente.java
@@ -1,6 +1,20 @@
 package Clinica;
 
-public class Serpiente {
+public class Serpiente extends Animal {
+private EspecieSerpiente especie;
+private boolean venenosa;
 
+public Serpiente(EspecieSerpiente especie, boolean venenosa, Sexo sexo, int peso, Propietario propietario) {
+	super(sexo, peso,propietario);
+	this.especie = especie;
+	this.venenosa = venenosa;
+}
+
+@Override
+public void contactarPropietario() {
+}
+System.out.println("Contactando al propietario de la serpiente");
+
+}
 	
 }
